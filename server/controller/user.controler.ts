8668dec4a6a1b767496b2245af26e60f3a522a9b
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-20 15:37:34 +0530

    check auth function done for now.

--- server/controller/user.controler.ts
+++ server/controller/user.controler.ts
@@ -201,9 +201,28 @@ export const resetPassword = async (req: Request, res: Response) => {
 
 export const checkAuth = async (req:Request, res:Response) => {
     try {
-        // const userId = 
+        const userId = req.id
+        const user = await User.findById(userId).select("-password")
+        if(!user){
+          return res.status(404).json({
+            success: false,
+            message: 'User not found'
+          })
+        }
+        return res.status(200).json({
+           success: true,
+           user
+        });
     } catch (error) {
         console.log(error);
     return res.status(500).json({ message: "Internal Server Error" });
     }
+}
+
+export const updateProfile = async (req:Request, res:Response) => {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
