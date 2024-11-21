9abb396508de7edf93fcd88efb21f29bce721743
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-21 18:16:11 +0530

    installed instamojo for payment gatways

---
+++ server/controller/order.controller.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import { Marketplace } from "../models/marketplace.model";
+import { Order } from "../models/order.model";
+import Stripe from "stripe";
+
